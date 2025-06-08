5f0bc60fe8e4088b5e4b548490d31ee63847ee9e
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-06-08 17:41:11 -0500

    Fix that.

--- src/main/java/co/casterlabs/quark/Bootstrap.java
+++ src/main/java/co/casterlabs/quark/Bootstrap.java
@@ -7,7 +7,7 @@ import co.casterlabs.quark.ingest.protocols.rtmp.RTMPServer;
 
 public class Bootstrap {
 
-    /* ffplay -x 1280 -y 720 -volume 50 http://localhost:8080/flv/test */
+    /* ffplay -x 1280 -y 720 -volume 50 http://localhost:8080/playback/flv/test */
     /* ffmpeg -stream_loop -1 -re -v debug -i test.flv -c copy -f flv rtmp://localhost/live/test */
     public static void main(String[] args) throws IOException {
         Quark.init();
