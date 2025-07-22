4411104a7afb171e06f23d756916a9f834f3a45c
Alex <33337309+e3ndr@users.noreply.github.com>
2025-07-22 08:57:48 -0500

    Server: Some comments :^)

--- server/src/main/java/co/casterlabs/quark/Quark.java
+++ server/src/main/java/co/casterlabs/quark/Quark.java
@@ -22,14 +22,33 @@ public class Quark {
     public static final boolean DEBUG = "true".equalsIgnoreCase(System.getenv("QUARK_DEBUG"));
     public static final String FFLL = DEBUG ? "level+warning" : "level+fatal";
 
+    /**
+     * HMAC256 secret for signed JWTs.
+     */
     public static final @Nullable String AUTH_SECRET = System.getenv("QUARK_AUTH_SECRET");
+    /**
+     * The default playback regex for anonymous users. Null to disallow anonymous
+     * viewership.
+     */
     public static final @Nullable String AUTH_ANON_PREGEX = System.getenv("QUARK_ANON_PREGEX");
 
+    /**
+     * -1 to disable.
+     */
     public static final int HTTP_PORT = Integer.parseInt(System.getenv().getOrDefault("HTTP_PORT", "8080"));
+    /**
+     * -1 to disable.
+     */
     public static final int RTMP_PORT = Integer.parseInt(System.getenv().getOrDefault("RTMP_PORT", "1935"));
 
+    /**
+     * Null to disable webhooks.
+     */
     public static final @Nullable String WEBHOOK_URL = System.getenv("QUARK_WEBHOOK_URL");
 
+    /**
+     * The interval (in seconds) in which a thumbnail should be rendered.
+     */
     public static final long THUMBNAIL_INTERVAL = TimeUnit.SECONDS.toMillis(Integer.parseInt(System.getenv().getOrDefault("QUARK_THUMB_IT", "30")));
 
     static {
